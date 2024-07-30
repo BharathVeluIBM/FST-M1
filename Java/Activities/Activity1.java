d3b2fffb6574254b002aa3b0422a1beb85baf1e5
BharathVeluIBM <141907503+BharathVeluIBM@users.noreply.github.com>
2024-07-30 18:10:08 +0530

    Add files via upload
    Java Activities

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,19 @@
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car honda =new Car();
+		
+		honda.make= 2014;
+		honda.color="Black";
+		honda.transmission="Manual";
+		
+		honda.displayCharacterstics();
+        honda.accelerate();
+        honda.brake();
+
+	}
+
+}
---
+++ Java/Activities/Activity6.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+class Plane {
+    private List<String> passengers;
+    private int maxPassengers;
+    private Date lastTimeTookOff;
+    private Date lastTimeLanded;
+
+    public Plane(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+        this.passengers = new ArrayList<>();
+    }
+
+    public void onboard(String passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public Date takeOff() {
+        this.lastTimeTookOff = new Date();
+        return lastTimeTookOff;
+    }
+
+    public void land() {
+        this.lastTimeLanded = new Date();
+        this.passengers.clear();
+    }
+
+    public Date getLastTimeLanded() {
+        return lastTimeLanded;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+}
+
+public class Activity6 {
+	 public static void main(String[] args) throws InterruptedException {
+	        Plane plane = new Plane(10);
+	        plane.onboard("BHARATH");
+	        plane.onboard("VELU");
+	        plane.onboard("RAMYA");
+	        System.out.println("Plane took off at: " + plane.takeOff());
+	        System.out.println("People on the plane: " + plane.getPassengers());
+	        Thread.sleep(5000);
+	        
+	        plane.land();
+	        
+	        System.out.println("Plane landed at: " + plane.getLastTimeLanded());
+	        System.out.println("People on the plane after landing: " + plane.getPassengers());
+	    }
+
+}
